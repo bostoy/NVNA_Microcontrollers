fb739a3dba76910d8febdc7f1f8be1442d720949
Borislav Stoyanov <bostoycontact@gmail.com>
2021-03-15 19:20:33 +0200

    Update ex5.c

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,8 +1,3 @@
-//TODO Добавете допълнителна обработка,
-// като показвате тенденцията при последното изменение,
-// както и запазвате максималната и минималната достигнати стойности.
-
-
 #define LED_PIN 10
 #define ANALOG_PIN A0
 #define BIT_RATE 9600
@@ -11,6 +6,7 @@ int sensorValue = 0;
 int previousValue= 0;
 int minValue = 1023;
 int maxValue = 0;
+int lastChange = 0;
 
 void setup()
 {
@@ -22,9 +18,9 @@ void setup()
 
 void loop()
 {
-  // read the value from the sensor
+ 
   sensorValue = analogRead(ANALOG_PIN);
-  // turn the LED on
+  
   if(sensorValue >= SENSITIVITY_THRESHOLD){
    	LedOn();
  
@@ -36,23 +32,38 @@ void loop()
 }
 void LedOn(){
   digitalWrite(LED_PIN, HIGH);
-  delay(sensorValue); // Wait for sensorValue millisecond(s)
+  delay(sensorValue);
 }
 void LedOff(){
  digitalWrite(LED_PIN, LOW);
-  delay(sensorValue); // Wait for sensorValue millisecond(s)
+  delay(sensorValue); 
 }
 
 void displayState(int currentValue){
  
   float resistance = 0;
+  /* if(previousValue >= currentValue) {
+    
+  	lastChange = previousValue - currentValue;
+  }else{
+    lastChange = currentValue-previousvalue;
+  }; */
+  
+  
   if(previousValue != currentValue){
     previousValue = currentValue;
       resistance =  convertResistance(currentValue);
       Serial.println(resistance);
       Serial.println(previousValue);
-	
   }
+     if(previousValue <= minValue){
+      minValue = previousValue;
+    }
+    
+    if(previousValue >= maxValue){
+      maxValue = previousValue;                         
+    }
+  
 }
 
 float convertResistance(int value){
